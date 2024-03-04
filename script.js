11d2d8a4f3e6b13ee5a79544ef355c0b8d010004
prof Edney <111137546+profedney@users.noreply.github.com>
2024-03-04 12:09:08 -0300

    add js

---
+++ script.js
@@ -0,0 +1,7 @@
+function mudaTexto(){
+    document.getElementById("paragrafo1").innerHTML = "Hello JavaScript!"   
+
+}
+function mostraHora(){
+    document.getElementById('paragrafo2').innerHTML = Date()
+}
